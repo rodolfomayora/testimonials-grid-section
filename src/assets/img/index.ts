46daeb78fb58b6b5c3ecc14c1a9727aa2ce013bf
Rodolfo Mayora <rodolfomayora96@gmail.com>
2021-03-19 05:51:27 -0400

    Proyect Configuration

---
+++ src/assets/img/index.ts
@@ -0,0 +1,4 @@
+// for STATIC: export { default as customName } from 'path';
+// for SVG   : export { ReactComponent as customName } from 'path';
+
+export { default as userPicture1 } from './static/image-daniel.jpg';
